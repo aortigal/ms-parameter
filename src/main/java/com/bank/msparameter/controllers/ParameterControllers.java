6930038b5fd9658f7bbeabddfab8a5855bd5f9e8
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-18 13:21:39 -0500

    add actuator

--- src/main/java/com/bank/msparameter/controllers/ParameterControllers.java
+++ src/main/java/com/bank/msparameter/controllers/ParameterControllers.java
@@ -1,103 +1,49 @@
 package com.bank.msparameter.controllers;
 
 import com.bank.msparameter.handler.ResponseHandler;
-import com.bank.msparameter.models.dao.ParameterDao;
 import com.bank.msparameter.models.documents.Parameter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.bank.msparameter.services.ParameterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/parameter")
 public class ParameterControllers {
 
     @Autowired
-    private ParameterDao dao;
-
-    private static final Logger log = LoggerFactory.getLogger(ParameterControllers.class);
+    private ParameterService parameterService;
 
     @PostMapping
-    public Mono<ResponseEntity<Object>> Create(@Valid @RequestBody Parameter p) {
-        log.info("[INI] Create Parameter");
-        p.setDateRegister(LocalDateTime.now());
-        return dao.save(p)
-                .doOnNext(parameter -> log.info(parameter.toString()))
-                .map(parameter -> ResponseHandler.response("Done", HttpStatus.OK, parameter))
-                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)))
-                .doFinally(fin -> log.info("[END] Create Parameter"));
+    public Mono<ResponseHandler> create(@Valid @RequestBody Parameter p) {
+        return parameterService.create(p);
     }
 
     @GetMapping
-    public Mono<ResponseEntity<Object>> FindAll() {
-        log.info("[INI] FindAll Parameter");
-        return dao.findAll()
-                .doOnNext(parameter -> log.info(parameter.toString()))
-                .collectList().map(parameters -> ResponseHandler.response("Done", HttpStatus.OK, parameters))
-                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)))
-                .doFinally(fin -> log.info("[END] FindAll Parameter"));
+    public Mono<ResponseHandler> findAll() {
+        return parameterService.findAll();
 
     }
 
     @GetMapping("/{id}")
-    public Mono<ResponseEntity<Object>> Find(@PathVariable String id) {
-        log.info("[INI] Find Parameter");
-        return dao.findById(id)
-                .doOnNext(parameter -> log.info(parameter.toString()))
-                .map(parameter -> ResponseHandler.response("Done", HttpStatus.OK, parameter))
-                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)))
-                .doFinally(fin -> log.info("[END] Find Parameter"));
+    public Mono<ResponseHandler> find(@PathVariable String id) {
+        return parameterService.find(id);
     }
 
     @PutMapping("/{id}")
-    public Mono<ResponseEntity<Object>> Update(@PathVariable("id") String id,@Valid @RequestBody Parameter p) {
-        log.info("[INI] Update Parameter");
-        return dao.existsById(id).flatMap(check -> {
-                    if (check){
-                        p.setDateUpdate(LocalDateTime.now());
-                        return dao.save(p)
-                                .doOnNext(parameter -> log.info(parameter.toString()))
-                                .map(parameter -> ResponseHandler.response("Done", HttpStatus.OK, parameter))
-                                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)));
-                    }
-                    else
-                        return Mono.just(ResponseHandler.response("Not found", HttpStatus.NOT_FOUND, null));
-
-                })
-                .doFinally(fin -> log.info("[END] Update Parameter"));
+    public Mono<ResponseHandler> update(@PathVariable("id") String id,@Valid @RequestBody Parameter p) {
+        return parameterService.update(id,p);
     }
 
     @DeleteMapping("/{id}")
-    public Mono<ResponseEntity<Object>> Delete(@PathVariable("id") String id) {
-        log.info("[INI] Delete Parameter");
-        return dao.existsById(id).flatMap(check -> {
-                    if (check)
-                        return dao.deleteById(id).then(Mono.just(ResponseHandler.response("Done", HttpStatus.OK, null)));
-                    else
-                        return Mono.just(ResponseHandler.response("Not found", HttpStatus.NOT_FOUND, null));
-                })
-                .doFinally(fin -> log.info("[END] Delete Parameter"));
+    public Mono<ResponseHandler> delete(@PathVariable("id") String id) {
+        return parameterService.delete(id);
     }
 
     @GetMapping(value ={"/catalogue/{code}","/catalogue/{code}/{value}"})
-    public Mono<ResponseEntity<Object>> FindByCode(@PathVariable String code,@PathVariable(required = false) String value) {
-        log.info("[INI] FindByCode Parameter");
-
-        Flux<Parameter> parameters = dao.findAll();
-
-        return parameters
-                .filter(p -> p.getCode().toString().equals(code) && (value!=null?p.getValue().equals(value):true))
-                .doOnNext(p -> log.info(p.toString()))
-                .collectList().map(p -> ResponseHandler.response("Done", HttpStatus.OK, p))
-                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)))
-                .doFinally(fin -> log.info("[END] FindByCode Parameter"));
-
+    public Mono<ResponseHandler> findByCode(@PathVariable String code,@PathVariable(required = false) String value) {
+        return parameterService.findByCode(code, value);
     }
 }
